7b0ecbcd4ecb0fb56a1a04fb649807cf49742e66
Ancientshield <ancientsword520@gmail.com>
2022-12-20 14:27:55 +0800

    belongsToMany

---
+++ database/migrations/2022_12_20_061017_create_table_author_book.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // belongsToMany兩個Table的關聯，第一個字母單數a->z
+        Schema::create('author_book', function (Blueprint $table) {
+            $table->id();
+            $table->integer('book_id')->nullable();
+            $table->integer('author_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_author_book');
+    }
+};
