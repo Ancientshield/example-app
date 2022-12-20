1d22ab9b9ba4077cf6a2adeba42c310b1350f36d
Ancientshield <ancientsword520@gmail.com>
2022-12-20 09:31:22 +0800

    add Book tables

--- database/migrations/2022_12_20_012558_create_books_table.php
+++ database/migrations/2022_12_20_012558_create_books_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string(column: 'title');
+            $table->text(column: 'description');
+            $table->decimal(column: 'price');
+            $table->boolean(column: 'available')->default(vaule: false);
             $table->timestamps();
         });
     }
