44f0d6c7337aed4848115f0fa6c3c94470c2d976
Ancientshield <ancientsword520@gmail.com>
2022-12-20 15:44:22 +0800

    DBseed

---
+++ database/seeders/Storeseeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Storeseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Store::factory()->count(3)->create();
+    }
+}
