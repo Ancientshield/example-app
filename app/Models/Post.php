ccbf7c8987cf1352c1031394999641c78411a866
Ancientshield <ancientsword520@gmail.com>
2022-12-20 09:21:57 +0800

    modify Model default behavior and read table name

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    //protected $table = "user_posts";
+    // 覆寫Model預設行為
+    //protected $primaryKey = "post_id";
+    // 覆寫Model預設讀取table名稱，原本應該為id
 }
